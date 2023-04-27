046043b9f4f28510ae7cffe3894fd39297b800f9
tanisha-singhal <tanusinghal1000@gmail.com>
2023-04-27 22:04:02 +0530

    deploy

--- frontend/src/components/UI/Password/EditPassword.js
+++ frontend/src/components/UI/Password/EditPassword.js
@@ -1,20 +1,47 @@
 import React from 'react'
 import classes from './EditPassword.module.css';
 import axios from 'axios';
-import { CloseIcon } from '@chakra-ui/icons';
+import { CloseIcon} from '@chakra-ui/icons';
+import { useToast } from '@chakra-ui/react';
 function EditPassword(props) {
     const { isOpen, onClose,newPassword,oldPassword, children } = props;
      const user=JSON.parse(localStorage.getItem("user"));
-
+     const toast=useToast();
     const updatePassword=async ()=>{
+      if (!newPassword || !oldPassword) {
+        toast({
+          title: "Please Fill all the feilds",
+          status: "warning",
+          duration: 5000,
+          isClosable: true,
+          position: "bottom",
+        });
+        return;
+      }
        await axios.post(`${process.env.REACT_APP_API_LINK}/api/user/updatePassword/${user._id}`,{newPassword,oldPassword},{
         headers:{
             tokenId:localStorage.getItem("userInfo"),
         }
        }).then(function (res){
             console.log(res);
+            toast({
+              title: "Password Updated.",
+              status: "success",
+              duration: 5000,
+              isClosable: true,
+              position: "bottom",
+            });
        }).catch(function (err){
+        toast({
+          title: "Error Occured",
+          description:err.response.data.error,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+          position: "bottom",
+        });
         console.log(err);
+
        })
        
        }
